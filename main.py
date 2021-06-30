f7e3d78567ac7b5cad3fc3fd14be348831e6a4d4
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-06-29 20:59:35 -0500

    Basic Imports, ending call, and initial instantiation.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 #!/bin/bash/python3
+
+# We could probably just use sockets but for ease of use we will use flask to serve, and since the requirements
+# require json interaction we will import jsonify as well
+from flask import Flask
+from flask import jsonify
+
+# Instatiate flask server
+app = Flask(__name__)
+
+# Set up one endpoint
+@app.route('/')
+def hello_world():
+    # Return JSON body
+    return jsonify(body='Land')
+
+# Run Code
+if __name__ == '__main__':
+    app.run()
