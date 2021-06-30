4324f722717c2aab5d34926079dad07df94846fd
DocLock17 <47020392+DocLock17@users.noreply.github.com>
2021-06-29 21:22:37 -0500

    Clean up endpoint and mix missing import

--- main.py
+++ main.py
@@ -4,15 +4,17 @@
 # require json interaction we will import jsonify as well
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 # Instatiate flask server
 app = Flask(__name__)
 
 # Set up one endpoint
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET'])
+def landing():
     # Return JSON body
-    return jsonify(body='Land')
+    return """Welcome to PalindREST, a simple REST API for reversing a string. Submit a PUT request using the 'input' \
+    KEY and a string VALUE of your choice and the reverse string will be returned as the VALUE of the 'body' KEY.""", 200
 
 # Final PUT endpoint
 @app.route('/', methods=['PUT'])
